c86fd0b71e79e4ad64a8e8cef4d2516a2bbe697d
Daniel <98242025+devoliveira61@users.noreply.github.com>
2022-02-16 16:24:14 -0300

    Update index.js

--- modulo1/strings-arrays/Exercício de Escrita de Código/index.js
+++ modulo1/strings-arrays/Exercício de Escrita de Código/index.js
@@ -15,14 +15,16 @@ console.log(`Essas são as minhas comidas preferidas: ${minhasComidasFavoritas} 
 
 alert('Olá, você deverá informar três tarefas que realizará hoje!')
 
-const tarefasUsuario = [];
+let listaDeTarefas = [];
 
-const tarefaUsuario1 = prompt('Informe a primeira (0) tarefa a ser realizada hoje: ');
-const tarefaUsuario2 = prompt('Informe a segunda (1) tarefa a ser realizada hoje: ');
-const tarefaUsuario3 = prompt('Informe a terceira (2) tarefa a ser realizada hoje: ');
-const listaDeTarefas = [tarefaUsuario1, tarefaUsuario2, tarefaUsuario3];
+const tarefaNum1 = prompt('Informe a primeira (0) tarefa a ser realizada hoje: ');
+const tarefaNum2 = prompt('Informe a segunda (1) tarefa a ser realizada hoje: ');
+const tarefaNum3 = prompt('Informe a terceira (2) tarefa a ser realizada hoje: ');
+
+listaDeTarefas.push(tarefaNum1, tarefaNum2, tarefaNum3);
 console.log(listaDeTarefas);
 
-
 const tarefaRealizada = Number(prompt('Digite o indice da tarefa que você já realizou 0, 1, 2:'));
+listaDeTarefas.splice(tarefaRealizada, 1);
+console.log(listaDeTarefas);
 
