c6dfb8ae13b4d0ab49e5f7b23b168376357e3141
Daniel Oliveira <devdoliveiradd@gmail.com>
2022-02-23 17:11:08 -0300

    Lacos

---
+++ modulo1/lacos/Exercício de Código/index.js
@@ -0,0 +1,34 @@
+// 1
+// const nomePets = [];
+// const quantosPets = Number(prompt("Quantos pet's você tem?"));
+// if (quantosPets === 0) {
+//   console.log("Que pena, você pode adotar um pet!");
+// } else if (quantosPets > 0) {
+//   let i = 0;
+//   while (i < quantosPets) {
+//     nomePets.push(prompt("Qual o nome dos seus pets?"));
+//     i++;
+//   }
+//   console.log(nomePets);
+// }
+
+// 2
+// const myArr = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
+// for (const i of myArr) {
+//   console.log(i);
+// }
+// for (const i of myArr) {
+//   console.log(i / 10);
+// }
+// let newArr = [];
+// for (const i of myArr) {
+//   if (i % 2 === 0) {
+//     newArr.push(i);
+//   }
+// }
+// console.log(newArr);
+// let newArrTwo = [];
+// for (const i of myArr) {
+//   newArrTwo.push(`O index: ${myArr.indexOf(i)} = ${i}`);
+// }
+// console.log(newArrTwo);
---
+++ modulo1/lacos/Exercício de Interpretação/index.js
@@ -0,0 +1,38 @@
+//1 
+
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)
+// Ele passará entre valor e i, e juntará a somatoria total dos dois e imprimirá no final. 
+
+//2 
+
+// let indice = 0;
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero, "indice", indice);
+// 	}
+//     indice ++;
+// }
+// Será impresso todas as arrays maiores que 18
+
+//3 
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "*"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+//Será impresso o valor "*" vezes o número informado. No exemplo de 4, sairia:
+// *
+// **
+// ***
+// ****
