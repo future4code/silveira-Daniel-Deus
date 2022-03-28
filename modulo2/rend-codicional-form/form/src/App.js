a85f8f66052ea47378a82fd8e3dc88a128cd3b4e
Daniel Oliveira <devdoliveiradd@gmail.com>
2022-03-28 16:09:29 -0300

    form

---
+++ modulo2/rend-codicional-form/form/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { FormI, FormNome } from './components/FormI';
+
+function App() {
+  return (
+    <div className="App">
+      <FormI />
+      <br></br>
+      <button>Próxima Etapa</button>
+    </div>
+  );
+}
+
+
+export default App;
